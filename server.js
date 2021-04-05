130186a717b6db3d342dbd659d2553568aaecae3
Shinji-Nishikita <pizattu@gmail.com>
2021-04-05 18:19:26 +0900

    installed spread sheet and showed follower and mediacount

--- server.js
+++ server.js
@@ -4,9 +4,34 @@ const cors = require("cors");
 const app = express();
 // const knex = require("./db/knex");
 const PORT = process.env.PORT || 3000;
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const credentials = require("./credentials.json")
 
 app.use(express.json());
 app.use(cors());
+
+//google spread sheet
+(async function() {
+   
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREAD_SHEET_URL);
+    
+    await doc.useServiceAccountAuth(credentials);
+    
+    await doc.loadInfo(); // loads document properties and worksheets
+ 
+    // console.log(doc.title)
+    const sheet = doc.sheetsByIndex[0]; 
+    // console.log("sheet is", sheet)
+    
+    const rows = await sheet.getRows();
+    // console.log(rows)
+    const eachRow = rows[0]._rawData
+    for (const ele of eachRow)
+    {
+        console.log(ele)
+    }
+   
+}())
 
 app.get("/pics", (req, res) =>
 {
@@ -22,3 +47,7 @@ app.listen(PORT, () =>
 {
     console.log(`🎉 Server running at https://localhost:${PORT}!`);
 })
+
+
+//APIからデータ取得 POSTするメソッドを探す(spread sheet用のメソッド)
+//フロントからfetchしてpost→サーバー側からそのpostされた情報をspreadsheetへ反映するメソッド実行
--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React, { Fragment, useEffect, useState } from "react";
-import logo from './logo.svg';
 import './App.css';
 
 function App()
 {
   const [view, setView] = useState([]);
-  console.log("view is",view)
+  const [follower, setFollower] = useState([]);
+  const [mediaCount, setMediaCount] = useState([]);
+  // console.log("view is", view)
+  // console.log("follower is", follower)
 
-  useEffect(async () => {
-    const result = await fetch(process.env.REACT_APP_API_KEY);
-    const obj = await result.json();
-    const myData = obj.media.data;
-    setView(myData);
+  useEffect(() => {
+    (async function()
+    {
+      const result = await fetch(process.env.REACT_APP_API_KEY);
+      const obj = await result.json();
+      const myData = obj.media.data;
+      console.log("myData is", myData)
+      setView(myData);
+
+      const result2 = await fetch(process.env.REACT_APP_FOLLOWER_KEY);
+      const obj2 = await result2.json();
+      const myfollower = obj2.business_discovery.followers_count
+      console.log("myfollower is", myfollower)
+      setFollower(myfollower);
+
+      const crrMediaCount = obj2.business_discovery.media_count
+      console.log("crrMediaCount is", crrMediaCount)
+      setMediaCount(crrMediaCount)
+    })()
+
+    // setFollower(myData);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Test</h2>
-        <ul>
+        <h2>My Instagram</h2>
+        <p>current follower is {follower}</p>
+        <p>current mediaCount is {mediaCount}</p>
+        <ul className="pictures">
           {view.map((val) => 
                 <img src={val.media_url} />
           )} 
