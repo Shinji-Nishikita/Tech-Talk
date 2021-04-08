2bf5d861951240e99b4c7100e0e9e8b2980216c6
Shinji-Nishikita <pizattu@gmail.com>
2021-04-08 15:25:55 +0900

    refactoring

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const knex = require("./db/knex");
 const PORT = process.env.PORT || 4000;
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const credentials = require("./credentials.json")
@@ -15,17 +14,6 @@ app.get("/pics", (req, res) =>
     res.send("test");
 })
 
-// app.post("/infos", async (req, res) =>
-// {
-//     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREAD_SHEET_URL);
-//     await doc.useServiceAccountAuth(credentials);
-//     await doc.loadInfo();
-//     const sheet = doc.sheetsByIndex[1];
-//     const sundar = sheet.addRow({ フォロワー数: req.body.business_discovery.followers_count, 投稿数: req.body.business_discovery.media_count });
-//     const rows = await sheet.getRows();
-//     res.send(req.body)
-// })
-
 app.post("/counts", async (req, res) =>
 {
     
@@ -35,15 +23,8 @@ app.post("/counts", async (req, res) =>
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
     const myData = req.body.business_discovery;
-    // console.log("myData",myData)
     const countData = req.body.business_discovery.media.data;
     await sheet.getRows();
-    // const columns = rows[0]._sheet.headerValues;
-            // const key = columns[index + 2];
-            // console.log("myData.media is",countData[0])
-    //         // await sheet.updateProperties({ post_no1: eachData[0].like_count });
-    // await sheet.addRow({ フォロワー数: myData.followers_count, 投稿数: myData.media_count, post_no1: countData[0].caption, post_no2: countData[1].caption, post_no3: countData[2].caption, post_no4: countData[3].caption, post_no5: countData[4].caption, post_no6:countData[5].caption });
-    // await sheet.addRow({ フォロワー数: myData.followers_count, 投稿数: myData.media_count});
 
     //フォロワー数、投稿数、投稿タイトル
     await sheet.addRow({ followers: myData.followers_count, posts: myData.media_count, post_no1: countData[0].caption, post_no2: countData[1].caption, post_no3: countData[2].caption, post_no4: countData[3].caption, post_no5: countData[4].caption, post_no6: countData[5].caption });
@@ -51,24 +32,95 @@ app.post("/counts", async (req, res) =>
     await sheet.addRow({ post_no1: countData[0].like_count, post_no2: countData[1].like_count, post_no3: countData[2].like_count, post_no4: countData[3].like_count, post_no5: countData[4].like_count, post_no6: countData[5].like_count });
     //コメント数
     await sheet.addRow({ post_no1: countData[0].comments_count, post_no2: countData[1].comments_count, post_no3: countData[2].comments_count, post_no4: countData[3].comments_count, post_no5: countData[4].comments_count, post_no6: countData[5].comments_count });
-    // console.log(countData[0].caption)
-            // await sheet.addRow({ post_no2: eachData[1].like_count });
-            // await sheet.addRow({ post_no3: eachData[2].like_count });
-            // await sheet.addRow({ post_no4: eachData[3].like_count });
-            // await sheet.addRow({ post_no5: eachData[4].like_count });
-            // await sheet.addRow({ post_no6: eachData[5].like_count });
-            return;
-    // console.log(rows[0]._sheet.headerValues); // 2
     res.send(req.body)
 })
-
-// app.use(express.static("./build"));
-// app.get("*", (req, res) => {
-//     res.sendFile("./build/index.html");
-// });
   
 app.listen(PORT, () =>
 {
     console.log(`🎉 Server running at https://localhost:${PORT}!`);
 })
+
+
+
+
+
+
+
+
+
+
+//保管用
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// // const knex = require("./db/knex");
+// const PORT = process.env.PORT || 4000;
+// const { GoogleSpreadsheet } = require('google-spreadsheet');
+// const credentials = require("./credentials.json")
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get("/pics", (req, res) =>
+// {
+//     res.send("test");
+// })
+
+// // app.post("/infos", async (req, res) =>
+// // {
+// //     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREAD_SHEET_URL);
+// //     await doc.useServiceAccountAuth(credentials);
+// //     await doc.loadInfo();
+// //     const sheet = doc.sheetsByIndex[1];
+// //     const sundar = sheet.addRow({ フォロワー数: req.body.business_discovery.followers_count, 投稿数: req.body.business_discovery.media_count });
+// //     const rows = await sheet.getRows();
+// //     res.send(req.body)
+// // })
+
+// app.post("/counts", async (req, res) =>
+// {
+    
+//     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREAD_SHEET_URL);
+//     console.log(process.env.GOOGLE_SPREAD_SHEET_URL)
+//     await doc.useServiceAccountAuth(credentials);
+//     await doc.loadInfo();
+//     const sheet = doc.sheetsByIndex[0];
+//     const myData = req.body.business_discovery;
+//     // console.log("myData",myData)
+//     const countData = req.body.business_discovery.media.data;
+//     await sheet.getRows();
+//     // const columns = rows[0]._sheet.headerValues;
+//             // const key = columns[index + 2];
+//             // console.log("myData.media is",countData[0])
+//     //         // await sheet.updateProperties({ post_no1: eachData[0].like_count });
+//     // await sheet.addRow({ フォロワー数: myData.followers_count, 投稿数: myData.media_count, post_no1: countData[0].caption, post_no2: countData[1].caption, post_no3: countData[2].caption, post_no4: countData[3].caption, post_no5: countData[4].caption, post_no6:countData[5].caption });
+//     // await sheet.addRow({ フォロワー数: myData.followers_count, 投稿数: myData.media_count});
+
+//     //フォロワー数、投稿数、投稿タイトル
+//     await sheet.addRow({ followers: myData.followers_count, posts: myData.media_count, post_no1: countData[0].caption, post_no2: countData[1].caption, post_no3: countData[2].caption, post_no4: countData[3].caption, post_no5: countData[4].caption, post_no6: countData[5].caption });
+//     //いいね数
+//     await sheet.addRow({ post_no1: countData[0].like_count, post_no2: countData[1].like_count, post_no3: countData[2].like_count, post_no4: countData[3].like_count, post_no5: countData[4].like_count, post_no6: countData[5].like_count });
+//     //コメント数
+//     await sheet.addRow({ post_no1: countData[0].comments_count, post_no2: countData[1].comments_count, post_no3: countData[2].comments_count, post_no4: countData[3].comments_count, post_no5: countData[4].comments_count, post_no6: countData[5].comments_count });
+//     // console.log(countData[0].caption)
+//             // await sheet.addRow({ post_no2: eachData[1].like_count });
+//             // await sheet.addRow({ post_no3: eachData[2].like_count });
+//             // await sheet.addRow({ post_no4: eachData[3].like_count });
+//             // await sheet.addRow({ post_no5: eachData[4].like_count });
+//             // await sheet.addRow({ post_no6: eachData[5].like_count });
+//             return;
+//     // console.log(rows[0]._sheet.headerValues); // 2
+//     res.send(req.body)
+// })
+
+// // app.use(express.static("./build"));
+// // app.get("*", (req, res) => {
+// //     res.sendFile("./build/index.html");
+// // });
+  
+// app.listen(PORT, () =>
+// {
+//     console.log(`🎉 Server running at https://localhost:${PORT}!`);
+// })
 
--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 function App()
 {
   const [view, setView] = useState([]);
-  const [follower, setFollower] = useState([]);
-  const [mediaCount, setMediaCount] = useState([]);
   const [instaData, setinstaData] = useState([]);
 
   useEffect(() =>
@@ -14,23 +12,14 @@ function App()
     async function getInsta()
     {
       const result = await axios.get(process.env.REACT_APP_FOLLOWER_KEY)
-      // console.log(result.data)
       setinstaData(result.data)
     }
     getInsta();
   }, []);
 
-  // console.log(instaData)
-
-  // async function handleClick()
-  // {
-  //   // console.log(instaData)
-  //   await axios.post("/infos", instaData)
-  // }
 
   async function handleClick2()
   {
-    console.log(instaData)
     await axios.post("/counts", instaData)
   }
   
@@ -50,10 +39,9 @@ function App()
     <div className="App">
       <header className="App-header">
         <h2 className="myinsta">My Instagram</h2>
-        {/* <button onClick={handleClick}>push</button> */}
         <button onClick={handleClick2}>show data</button>
         <ul className="pictures">
-          {view.map((val, index) =>
+          {view.map((val) =>
             <img src={val.media_url} />
             )} 
       </ul>
@@ -63,4 +51,84 @@ function App()
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//保管用
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import './App.css';
+
+// function App()
+// {
+//   const [view, setView] = useState([]);
+//   const [follower, setFollower] = useState([]);
+//   const [mediaCount, setMediaCount] = useState([]);
+//   const [instaData, setinstaData] = useState([]);
+
+//   useEffect(() =>
+//   {
+//     async function getInsta()
+//     {
+//       const result = await axios.get(process.env.REACT_APP_FOLLOWER_KEY)
+//       // console.log(result.data)
+//       setinstaData(result.data)
+//     }
+//     getInsta();
+//   }, []);
+
+//   // console.log(instaData)
+
+//   // async function handleClick()
+//   // {
+//   //   // console.log(instaData)
+//   //   await axios.post("/infos", instaData)
+//   // }
+
+//   async function handleClick2()
+//   {
+//     console.log(instaData)
+//     await axios.post("/counts", instaData)
+//   }
+  
+
+//   useEffect(() => {
+//     (async function()
+//     {
+//       const result = await fetch(process.env.REACT_APP_API_KEY);
+//       const obj = await result.json();
+//       const myData = obj.media.data;
+//       setView(myData);
+//     })()
+
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h2 className="myinsta">My Instagram</h2>
+//         {/* <button onClick={handleClick}>push</button> */}
+//         <button onClick={handleClick2}>show data</button>
+//         <ul className="pictures">
+//           {view.map((val, index) =>
+//             <img src={val.media_url} />
+//             )} 
+//       </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
